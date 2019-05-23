778ecbb7abe0d145d387677dc1266459e081bf97
sergio-hernandez <sergio_nandez@hotmail.com>
2019-05-23 16:57:21 -0500

    Add files via upload
    Microsimulation laboratory codes: only synthetic database construction.

---
+++ HealthMicroSim/01Code/0Lab/03LabSinth.R
@@ -0,0 +1,7 @@
+#*******************************************
+#                Master code----
+#*******************************************
+
+# This could should source all other files
+# required for the microsimulation execution
+# and results.
---
+++ HealthMicroSim/01Code/1Demography/4Synthetic/1DemoSynth.R
@@ -0,0 +1,9 @@
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# 
+# Demographic synthetic database             
+#
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# This could contains, executes and builds
+# the synthetic database with all demographic
+# information.
